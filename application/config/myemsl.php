f11618db309ef2fbe980c1faf10c391f7c33c0e7
Ken Auberry <kenneth.auberry@pnnl.gov>
2015-11-16 16:44:15 -0800

    Initial Project Commit

---
+++ application/config/myemsl.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['myemsl_config_file_path'] = "/etc/myemsl/";
+
+$config['template'] = "emsl";
+$config['site_color'] = 'orange';
+$config['jquery_script'] = "jquery-1.11.2.js";
