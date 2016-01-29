943a727304f11532b7fa087051fc169b07a77c0a
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-01-29 09:12:59 -0800

    Working implementation of Ajax object loader code

--- application/controllers/Reporting.php
+++ application/controllers/Reporting.php
@@ -4,7 +4,7 @@ require_once('Baseline_controller.php');
 
 class Reporting extends Baseline_controller {
   public $last_update_time;
-  
+  public $accepted_object_types;
   
   function __construct() {
     parent::__construct();
@@ -12,6 +12,7 @@ class Reporting extends Baseline_controller {
     $this->load->library('myemsl-eus-library/EUS','','eus');
     $this->load->helper(array('network','file_info','inflector'));
     $this->last_update_time = get_last_update(APPPATH);
+    $this->accepted_object_types = array('instrument','user','proposal');
   }
 
 
@@ -28,15 +29,13 @@ class Reporting extends Baseline_controller {
     }
     $this->page_data['page_header'] = "MyEMSL Uploads per ".ucwords($object_type);
     $this->page_data['css_uris'] = array(
-      // "/resources/stylesheets/status.css",
       "/resources/stylesheets/status_style.css",
       "/resources/scripts/select2/select2.css",
       base_url()."resources/stylesheets/reporting.css"   
     );
     $this->page_data['script_uris'] = array(
       "/resources/scripts/spinner/spin.min.js",
-      // "/resources/scripts/jquery-crypt/jquery.crypt.js",
-      // "/resources/scripts/select2/select2.js",
+      "/resources/scripts/spinner/jquery.spin.js",
       "/resources/scripts/moment.min.js",
       base_url()."resources/scripts/highcharts/js/highcharts.js"
     );
@@ -47,46 +46,43 @@ class Reporting extends Baseline_controller {
     if(!empty($default_object_id) && in_array($default_object_id,$object_list)){
       $object_list = array(strval($default_object_id));
     }
-    $transaction_info = array();
+    // $transaction_info = array();
     $object_info = $this->eus->get_object_info($object_list,$object_type);
-    $transaction_retrieval_func = "summarize_uploads_by_{$object_type}";
+    // $transaction_retrieval_func = "summarize_uploads_by_{$object_type}";
     foreach($object_list as $object_id){
-      $transaction_info[$object_id] = $this->rep->$transaction_retrieval_func($object_id,'2015-10-01','2015-12-01');
+      // $transaction_info[$object_id] = $this->rep->$transaction_retrieval_func($object_id,'2015-10-01','2015-12-01');
+      $this->page_data['placeholder_info'][$object_id] = array(
+        'object_type' => $object_type,
+        'object_id' => $object_id
+      );
     }
     
-    $this->page_data['content_view'] = "selection_prefs/{$object_type}.html";
+    $this->page_data['content_view'] = "object_types/{$object_type}.html";
     $this->page_data['my_objects'] = $object_info;
-    $this->page_data['transaction_info'] = $transaction_info;
+    // $this->page_data['transaction_info'] = $transaction_info;
     
     $this->load->view('reporting_view.html',$this->page_data);
   }
   
-  
-  
-  public function preferences($object_type){
-    $accepted_object_types = array('instrument','proposal','user');
-    if(!in_array($object_type,$accepted_object_types)){
-      //error 404
-    }
-    $my_objects = $this->rep->get_selected_objects($this->user_id,$object_type);
-    $object_list = array_map('strval', array_keys($my_objects[$object_type]));
     
-    $object_info = $this->eus->get_object_info($object_list,$object_type);
-    
-    $this->page_data['page_header'] = "Select ".ucwords($object_type)."s of Interest";
-    $this->page_data['content_view'] = "selection_prefs/{$object_type}.html";
-    $this->page_data['my_objects'] = $my_objects;
-    $this->page_data['object_info'] = $object_info;
-    $this->load->view('reporting_view.html',$this->page_data);
-  }
-  
-  
-  
-  
-  
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* API functionality for Ajax calls from UI                  */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+  // Call to retrieve fill-in HTML for reporting block entries
+  public function get_reporting_info($object_type,$object_id,$start_date = false, $end_date = false){
+    $transaction_retrieval_func = "summarize_uploads_by_{$object_type}";
+    $transaction_info = array();
+    $transaction_info = $this->rep->$transaction_retrieval_func($object_id,$start_date,$end_date);
+    $this->page_data['transaction_info'] = $transaction_info;
+    $this->page_data["{$object_type}_id"] = $object_id;
+    $this->page_data['object_type'] = $object_type;
+    $this->load->view("object_types/{$object_type}_body_insert.html", $this->page_data);
+  }
+
+
+
+
   public function get_uploads_for_instrument($instrument_id,$start_date = false,$end_date = false){
     $results = $this->rep->summarize_uploads_by_instrument($instrument_id,$start_date,$end_date);
     $results_size = sizeof($results);
