7027bd86877ee27ee6f7a5efc02ad98df8c1f699
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-02-05 12:59:55 -0800

    Padding time graph viewer with range of dates

--- application/controllers/Reporting.php
+++ application/controllers/Reporting.php
@@ -5,7 +5,7 @@ require_once('Baseline_controller.php');
 class Reporting extends Baseline_controller {
   public $last_update_time;
   public $accepted_object_types;
-  
+
   function __construct() {
     parent::__construct();
     $this->load->model('Reporting_model','rep');
@@ -15,12 +15,12 @@ class Reporting extends Baseline_controller {
     $this->accepted_object_types = array('instrument','user','proposal');
   }
 
-
+  //testing uploade
 
   public function index(){
     redirect('reporting/view');
   }
-  
+
   public function view($object_type, $time_range = '1-month', $start_date = false, $end_date = false){
     $time_range = str_replace(array('-','_','+'),' ',$time_range);
     if(!strtotime($time_range)){
@@ -38,7 +38,7 @@ class Reporting extends Baseline_controller {
     }
     $times = $this->rep->canonicalize_date_range($start_date, $end_date);
     extract($times);
-    
+
     $object_type = singular($object_type);
     $accepted_object_types = array('instrument','proposal','user');
     if(!in_array($object_type,$accepted_object_types)){
@@ -48,7 +48,7 @@ class Reporting extends Baseline_controller {
     $this->page_data['css_uris'] = array(
       "/resources/stylesheets/status_style.css",
       "/resources/scripts/select2/select2.css",
-      base_url()."resources/stylesheets/reporting.css"   
+      base_url()."resources/stylesheets/reporting.css"
     );
     $this->page_data['script_uris'] = array(
       "/resources/scripts/spinner/spin.min.js",
@@ -56,7 +56,7 @@ class Reporting extends Baseline_controller {
       "/resources/scripts/moment.min.js",
       base_url()."resources/scripts/highcharts/js/highcharts.js"
     );
-    
+
     $this->page_data['my_objects'] = '';
     $my_object_list = $this->rep->get_selected_objects($this->user_id,$object_type);
     $object_list = array_map('strval', array_keys($my_object_list[$object_type]));
@@ -79,11 +79,11 @@ class Reporting extends Baseline_controller {
     $this->page_data['content_view'] = "object_types/{$object_type}.html";
     $this->page_data['my_objects'] = $object_info;
     // $this->page_data['transaction_info'] = $transaction_info;
-    
+
     $this->load->view('reporting_view.html',$this->page_data);
   }
-  
-    
+
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* API functionality for Ajax calls from UI                  */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -118,8 +118,8 @@ class Reporting extends Baseline_controller {
           'time_range' => $time_range,
           'message' => "<p>Using ".$end_date_object->format('Y-m-d')." as the new origin time</p>"
         );
-                    
-        
+
+
       }else{
         //looks like the time range is borked, pick the default
         $time_range = '1 week';
@@ -127,7 +127,7 @@ class Reporting extends Baseline_controller {
       }
     }else{ //time_range is apparently valid
       if(($valid_st || $valid_et) && !($valid_st && $valid_et)){
-        //looks like we want an offset time either start or finish 
+        //looks like we want an offset time either start or finish
         $times = time_range_to_date_pair($time_range,$latest_data_object,$start_date,$end_date);
       }else{
         $times = time_range_to_date_pair($time_range, $latest_data_object);
@@ -138,14 +138,14 @@ class Reporting extends Baseline_controller {
     // var_dump($times);
     // echo "</pre>";
     // $this->page_data['results_message'] .= $times['message'];
-    
+
     $transaction_retrieval_func = "summarize_uploads_by_{$object_type}";
     $transaction_info = array();
     $transaction_info = $this->rep->$transaction_retrieval_func($object_id,$start_date,$end_date, $with_timeline);
     // echo "<pre>";
     // var_dump($transaction_info);
     // echo "</pre>";
-    // exit;    
+    // exit;
     $this->page_data['transaction_info'] = $transaction_info;
     $this->page_data['object_id'] = $object_id;
     $this->page_data["{$object_type}_id"] = $object_id;
@@ -156,7 +156,7 @@ class Reporting extends Baseline_controller {
     // var_dump($this->page_data);
     // echo "</pre>";
     // exit;
-    
+
     if($with_timeline){
       $this->load->view("object_types/{$object_type}_body_insert.html", $this->page_data);
     }else{
@@ -174,22 +174,22 @@ class Reporting extends Baseline_controller {
     $status_message = '{$results_size} transaction{$pluralizer} returned';
     send_json_array($results);
   }
-  
+
   public function get_uploads_for_proposal($proposal_id,$start_date = false,$end_date = false){
     $results = $this->rep->summarize_uploads_by_proposal($proposal_id,$start_date,$end_date);
     send_json_array($results);
   }
-  
+
   public function get_uploads_for_user($eus_person_id, $start_date = false, $end_date = false){
     $results = $this->rep->summarize_uploads_by_user($eus_person_id,$start_date,$end_date);
     send_json_array($results);
   }
-  
+
   public function get_proposals($proposal_name_fragment, $active = 'active'){
     $results = $this->eus->get_proposals_by_name($proposal_name_fragment,$active);
     send_json_array($results);
   }
-  
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* Testing functionality                                     */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -199,46 +199,46 @@ class Reporting extends Baseline_controller {
     var_dump($results);
     echo "</pre>";
   }
-  
+
   public function test_get_uploads_for_user($eus_person_id,$start_date = false,$end_date = false){
     $results = $this->rep->summarize_uploads_by_user($eus_person_id,$start_date,$end_date);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
-    
+
   }
-  
+
   public function test_get_uploads_for_instrument($eus_instrument_id,$start_date = false,$end_date = false){
     $results = $this->rep->summarize_uploads_by_instrument($eus_instrument_id,$start_date,$end_date);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
-    
+
   }
 
-  
+
   public function test_get_selected_objects($eus_person_id){
     $results = $this->rep->get_selected_objects($eus_person_id);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
   }
-  
+
   public function test_get_object_list($object_type,$filter = ""){
     $results = $this->eus->get_object_list($object_type,$filter);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
   }
-  
+
   public function test_get_latest($object_type,$object_id){
     $results = $this->rep->latest_available_data($object_type,$object_id);
     echo "<pre>";
     var_dump($results);
     echo "</pre>";
-    
+
   }
-  
+
 
 }
 
--- application/models/Reporting_model.php
+++ application/models/Reporting_model.php
@@ -7,37 +7,37 @@
 /*                                                                             */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 class Reporting_model extends CI_Model {
-  
+
   // protected $selected_objects;
-  
+
   function __construct(){
     parent::__construct();
     define("TRANS_TABLE", 'transactions');
     define("FILES_TABLE", 'files');
     $this->load->database();
     $this->load->helper(array('item'));
-    
+
     //$this->selected_objects = $this->get_selected_objects($this->user_id);
   }
-  
-  
-  
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
- *    Publicly available API calls 
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    Publicly available API calls
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-  private function transactions_to_results($results, $make_day_graph = true){
+  private function transactions_to_results($results, $make_day_graph = true, $start_date = false, $end_date = false){
     //use the transaction list to retrieve summary info
     if(!empty($results['transactions'])){
       $results['transaction_info'] = $this->get_info_for_transactions($results['transactions']);
       $results = $this->generate_summary_data($results);
-      $results['day_graph'] = $this->generate_day_graph_summary($results['transactions']);
+      $results['day_graph'] = $this->generate_day_graph_summary($results['transactions'], $start_date, $end_date);
     }else{
       return array();
     }
     return $results;
   }
- 
- 
+
+
   function summarize_uploads_by_user($eus_person_id, $start_date, $end_date, $make_day_graph){
     //canonicalize start and end times (yields $start_time & $end_time)
     extract($this->canonicalize_date_range($start_date, $end_date));
@@ -47,29 +47,29 @@ class Reporting_model extends CI_Model {
       'transactions' => array(),
       'time_range' => array('start_time' => $start_time, 'end_time' => $end_time)
     );
-    
+
     $results['transactions'] = $this->get_transactions_for_user($eus_person_id, $start_time, $end_time, false);
     $results = $this->transactions_to_results($results, $make_day_graph);
-    
+
     return $results;
   }
-  
-  
-  
-  
+
+
+
+
   function summarize_uploads_by_proposal($eus_proposal_id, $start_date, $end_date, $make_day_graph){
     //canonicalize start and end times (yields $start_time & $end_time)
     extract($this->canonicalize_date_range($start_date, $end_date));
-    
+
     $results = array(
       'transactions' => array(),
       'time_range' => array('start_time' => $start_time, 'end_time' => $end_time)
     );
-    
+
     //get proposal_group_list
     $group_collection = $this->get_proposal_group_list($eus_proposal_id);
     $group_list = array_keys($group_collection);
-        
+
     //get transactions for time period & group_list
     $results['transactions'] = $this->get_transactions_from_group_list($group_list, $start_time, $end_time);
 
@@ -77,13 +77,12 @@ class Reporting_model extends CI_Model {
 
     return $results;
   }
-    
- 
- 
-  
+
+
+
+
   function summarize_uploads_by_instrument($eus_instrument_id, $start_date, $end_date, $make_day_graph){
     extract($this->canonicalize_date_range($start_date, $end_date));
-    
     //get instrument group_id list
     $group_collection = $this->get_instrument_group_list($eus_instrument_id);
     $group_list = array_keys($group_collection);
@@ -96,19 +95,20 @@ class Reporting_model extends CI_Model {
     );
     //get transactions for time period & group_list
     $results['transactions'] = $this->get_transactions_from_group_list($group_list, $start_time, $end_time);
-    $results = $this->transactions_to_results($results, $make_day_graph);
+    $results = $this->transactions_to_results($results, $make_day_graph, $start_time, $end_time);
 
     return $results;
   }
-  
-  
-  
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
- *    Private functionality for behind the scenes data retrieval 
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    Private functionality for behind the scenes data retrieval
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
   private function get_transactions_from_group_list($group_list, $start_time, $end_time){
     // extract($this->canonicalize_date_range($start_date, $end_date));
+    // echo "start => {$start_time}  end => {$end_time}";
     $transactions = array();
     $where_clause = array('stime >=' => $start_time);
     if($end_time){
@@ -145,7 +145,7 @@ class Reporting_model extends CI_Model {
     $this->db->where('ing.message','completed')->where('ing.person_id',$eus_user_id);
     $this->db->order_by('t.transaction desc');
     $transaction_query = $this->db->get();
-    
+
     if($transaction_query && $transaction_query->num_rows() > 0){
       foreach($transaction_query->result() as $row){
         $stime = date_create($row->submit_time);
@@ -156,9 +156,9 @@ class Reporting_model extends CI_Model {
     }
     return $transactions;
   }
-  
-  
-  
+
+
+
   private function get_info_for_transactions($transaction_info){
     //get proposals
     $transaction_list = array_keys($transaction_info);
@@ -175,7 +175,7 @@ class Reporting_model extends CI_Model {
         $trans_prop_lookup[$row->transaction]['eus_proposal_id'] = $row->proposal_id;
       }
     }
-    
+
     //get instruments
     $this->db->select(array('f.transaction','g.name as group_name','g.type as group_type'));
     $this->db->where("(g.type = 'omics.dms.instrument_id' or g.type ilike 'instrument.%')");
@@ -190,21 +190,21 @@ class Reporting_model extends CI_Model {
         $trans_prop_lookup[$row->transaction]['eus_instrument_id'] = $instrument_id + 0;
       }
     }
-    
+
     //get_users
     $this->db->select(array('person_id','trans_id as transaction'))->where_in('trans_id',$transaction_list)->order_by('step desc');
     $user_query = $this->db->get('ingest_state');
-    
+
     if($user_query && $user_query->num_rows() > 0){
       foreach($user_query->result() as $row){
         $trans_prop_lookup[$row->transaction]['eus_person_id'] = $row->person_id;
       }
     }
-    
+
     return $trans_prop_lookup;
-    
+
   }
-  
+
 
   private function generate_summary_data($results){
     $this->db->where_in('transaction', array_keys($results['transactions']));
@@ -223,7 +223,7 @@ class Reporting_model extends CI_Model {
       'instrument' => array(),
       'user' => array()
     );
-    
+
     $file_info_query = $this->db->get(FILES_TABLE);
     if($file_info_query && $file_info_query->num_rows() > 0){
       $transaction_records = array(
@@ -235,11 +235,11 @@ class Reporting_model extends CI_Model {
         if(array_key_exists($file_row->transaction, $results['transactions'])){
           $tx = $results['transactions'][$file_row->transaction];
           $tx_i = $results['transaction_info'][$file_row->transaction];
-          
+
           $tx['size_bytes'] = $file_row->file_size_in_bytes;
           $tx['size_string'] = format_bytes($file_row->file_size_in_bytes);
           $tx['count'] = $file_row->file_count;
-          
+
           if($file_row->file_size_in_bytes > $transaction_records['largest_upload']['file_size'] + 0){
             $transaction_records['largest_upload'] = array(
               'transaction_id' => $file_row->transaction,
@@ -252,16 +252,16 @@ class Reporting_model extends CI_Model {
               'file_count' => $file_row->file_count
             );
           }
-          
+
           $tx['eus_instrument_id'] = array_key_exists('eus_instrument_id',$tx_i) ? $tx_i['eus_instrument_id'] : 0;
           $tx['eus_proposal_id'] = array_key_exists('eus_proposal_id', $tx_i) ? $tx_i['eus_proposal_id'] : "0";
           $tx['eus_person_id'] = array_key_exists('eus_person_id', $tx_i) ? $tx_i['eus_person_id'] : "0";
-          
+
           $results['transactions'][$file_row->transaction] = $tx;
-          
+
           $total_file_count_summary += $file_row->file_count;
           $total_file_size_summary += $file_row->file_size_in_bytes;
-          
+
           //transaction based stats for user/instrument/proposal
           if(!array_key_exists($tx_i['eus_person_id'], $transaction_stats['user'])){
             $transaction_stats['user'][$tx_i['eus_person_id']] = 1;
@@ -278,7 +278,7 @@ class Reporting_model extends CI_Model {
           }else{
             $transaction_stats['proposal'][$tx_i['eus_proposal_id']] += 1;
           }
-          
+
         }
       }
     }
@@ -293,29 +293,42 @@ class Reporting_model extends CI_Model {
       'total_size_bytes' => $total_file_size_summary,
       'total_size_string' => format_bytes($total_file_size_summary)
     );
-        
+
     unset($results['transaction_info']);
-    
+
     return $results;
   }
-  
-  
-  
-  
-  /* 
+
+
+
+
+  /*
    * $transaction list is a list of transaction id's, each with an array containing submit time
    * size_bytes, size_string (opt), and count
    * */
   private function generate_day_graph_summary($transaction_list,$start_date = false,$end_date = false){
-    
+
+    // echo "start => {$start_date}  end => {$end_date}";
     //parse requested date range (if specified)
     $start_date = strtotime($start_date) ? date_create($start_date) : false;
     $end_date = strtotime($end_date) ? date_create($end_date) : false;
-    
-    
+
+    $by_date_list = array();
+    if($start_date && $end_date){
+      $now_date = clone $start_date;
+      while($now_date->getTimestamp() <= $end_date->getTimestamp()){
+        $date_key = $now_date->format('Y-m-d');
+        $by_date_list[$date_key] = array(
+          'file_size' => 0, 'file_count' => 0,
+          'upload_count' => 0, 'transaction_list' => array()
+        );
+        $now_date->modify('+1 day');
+      }
+    }
+
     $date_list = array();
-    $summary_list = array('by_date' => array(), 'by_hour_list' => array());
-    
+    $summary_list = array('by_date' => $by_date_list, 'by_hour_list' => array());
+
     //partition transaction entries into their appropriate upload days
     foreach($transaction_list as $trans_id => $info){
       if(strtotime($info['submit_time'])){  //is the submission time valid?
@@ -326,7 +339,7 @@ class Reporting_model extends CI_Model {
         $time_key = $time_key->setTime($time_key->format('H'),0,0)->format('H:i');
         if(!array_key_exists($date_key,$summary_list['by_date'])){
           $summary_list['by_date'][$date_key] = array(
-            'file_size' => $info['size_bytes'] + 0, 
+            'file_size' => $info['size_bytes'] + 0,
             'file_count' => $info['count'] + 0,
             'upload_count' => 1,
             'transaction_list' => array($trans_id)
@@ -347,6 +360,7 @@ class Reporting_model extends CI_Model {
     //$summary_times = range(0, 23, 1);
     ksort($summary_list['by_hour_list']);
     ksort($summary_list['by_date']);
+    var_dump($summary_list['by_date']);
     //foreach($summary_times as $key){
     foreach($summary_list['by_hour_list'] as $time_key => $info){
       $trans_count = sizeof($info['sizes']);
@@ -359,9 +373,8 @@ class Reporting_model extends CI_Model {
       );
     }
     unset($summary_list['by_hour_list']);
-    
     return $summary_list;
-    
+
   }
 
   public function latest_available_data($object_type, $object_id){
@@ -369,41 +382,41 @@ class Reporting_model extends CI_Model {
     return $this->$latest_function_name($object_id);
   }
 
-  
+
 
   private function latest_available_instrument_data($instrument_id){
     $group_collection = $this->get_instrument_group_list($instrument_id);
     $group_list = array_keys($group_collection);
-    
+
     $this->db->select('MAX(t.stime) as most_recent_upload');
     $this->db->from('group_items gi');
     $this->db->join('files f','gi.item_id = f.item_id');
     $this->db->join('transactions t', 'f.transaction = t.transaction');
     $this->db->where_in('gi.group_id',$group_list)->limit(1);
     $query = $this->db->get();
-    
+
     if($query && $query->num_rows() > 0){
-      if(strtotime($query->row()->most_recent_upload)){      
+      if(strtotime($query->row()->most_recent_upload)){
         $latest_time = $query->row()->most_recent_upload;
         $latest_time = new DateTime($latest_time);
       }
       return $latest_time->format('Y-m-d H:i');
     }
   }
-  
+
   private function latest_available_proposal_data($proposal_id){
     $group_collection = $this->get_proposal_group_list($proposal_id);
     $group_list = array_keys($group_collection);
-    
+
     $this->db->select('MAX(t.stime) as most_recent_upload');
     $this->db->from('group_items gi');
     $this->db->join('files f','gi.item_id = f.item_id');
     $this->db->join('transactions t', 'f.transaction = t.transaction');
     $this->db->where_in('gi.group_id',$group_list)->limit(1);
     $query = $this->db->get();
-    
+
     if($query && $query->num_rows() > 0){
-      if(strtotime($query->row()->most_recent_upload)){      
+      if(strtotime($query->row()->most_recent_upload)){
         $latest_time = $query->row()->most_recent_upload;
         $latest_time = new DateTime($latest_time);
       }
@@ -433,29 +446,29 @@ class Reporting_model extends CI_Model {
 
 
 
-  
+
   public function canonicalize_date_range($start_date, $end_date){
     //both start and end times are filled in and valid
     $start_date = $this->convert_short_date($start_date);
     $end_date = $this->convert_short_date($end_date, 'end');
-    
+
     $start_time = strtotime($start_date) ? date_create($start_date)->setTime(0,0,0) : date_create('1983-01-01 00:00:00');
     $end_time = strtotime($end_date) ? date_create($end_date)->setTime(23,59,59) : false;
-    
+
     if($end_time < $start_time && !empty($end_time)){
       //flipped??
       $temp_start = strtotime($end_time) ? clone($end_time) : false;
       $end_time = clone($start_time);
       $start_time = $temp_start;
     }
-    
+
     return array(
       'start_time_object' => $start_time, 'end_time_object' => $end_time,
       'start_time' => $start_time->format('Y-m-d H:i:s'),
       'end_time' => $end_time->format('Y-m-d H:i:s')
     );
   }
-  
+
   private function convert_short_date($date_string, $type = 'start'){
     if(preg_match('/(\d{4})$/',$date_string,$matches)){
       //looks like just a year
@@ -465,24 +478,24 @@ class Reporting_model extends CI_Model {
     }
     return $date_string;
   }
-  
+
   private function get_proposal_group_list($proposal_id_filter = ""){
     $this->db->select(array('group_id','name as proposal_id'))->where('type','proposal');
     if(!empty($proposal_id_filter)){
       $this->db->where('name',$proposal_id_filter);
     }
     $query = $this->db->get('groups');
-    
+
     $results_by_proposal = array();
     if($query && $query->num_rows()){
       foreach($query->result() as $row){
         $results_by_proposal[$row->group_id] = $row->proposal_id;
       }
     }
-    return $results_by_proposal;  
+    return $results_by_proposal;
   }
-  
-  
+
+
   private function get_instrument_group_list($inst_id_filter = ""){
     $this->db->select(array('group_id','name','type'));
     // if(!empty($inst_id_filter) && intval($inst_id_filter) >= 0){
@@ -490,7 +503,7 @@ class Reporting_model extends CI_Model {
     // }else{
       $where_clause = "(type = 'omics.dms.instrument_id' or type ilike 'instrument.%') and name not in ('foo')";
     // }
-    
+
     $this->db->where($where_clause);
     $query = $this->db->order_by('name')->get('groups');
     $results_by_inst_id = array();
@@ -511,11 +524,11 @@ class Reporting_model extends CI_Model {
     }else{
       $results = $results_by_inst_id;
     }
-    
+
     return $results;
   }
-  
-  
+
+
   public function get_selected_objects($eus_person_id,$restrict_type = false){
     $DB_prefs = $this->load->database('website_prefs',TRUE);
     $DB_prefs->select(array('eus_person_id','item_type','item_id'));
@@ -537,7 +550,7 @@ class Reporting_model extends CI_Model {
             break;
           default:
             $group_list = $row->item_id;
-            
+
         }
         $item_id = "{$row->item_id}";
         $results[$row->item_type][$item_id] = $group_list;
@@ -545,9 +558,9 @@ class Reporting_model extends CI_Model {
     }
     return $results;
   }
-  
-  
-  
-  
+
+
+
+
 }
 ?>
