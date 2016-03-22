38dd5e964b1e09245346cc9973fe39b4545f21af
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-03-22 12:49:14 -0700

    Changes to handle deployment issues on dev2

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 if(!defined('BASEPATH'))
-  exit('No direct script access allowed');  
+  exit('No direct script access allowed');
 // --------------------------------------------------------------------
 
 function get_user(){
-  $user = '(unknown)';
+  $user = false;
   if(isset($_SERVER["REMOTE_USER"])){
-    $user = str_replace('@PNL.GOV','',$_SERVER["REMOTE_USER"]);
+    $user = strtolower(str_replace('@PNL.GOV','',$_SERVER["REMOTE_USER"]));
   }
-  return strtolower($user);
+  return $user;
 }
 
-function get_user_details($user_id){
-  $user_info = array(
-    'user_id' => strtolower($_SERVER['REMOTE_USER']),
-    'first_name' => $_SERVER['LDAP_GIVENNAME'],
-    'middle_initial' => $_SERVER['LDAP_INITIALS'],
-    'last_name' => $_SERVER['LDAP_SN'],
-    'email' => strtolower($_SERVER['LDAP_MAIL'])    
-  );
-  return $user_info;
-}
+// function get_user_details($user_id){
+//   $user_info = array(
+//     'user_id' => strtolower($_SERVER['REMOTE_USER']),
+//     'first_name' => $_SERVER['LDAP_GIVENNAME'],
+//     'middle_initial' => $_SERVER['LDAP_INITIALS'],
+//     'last_name' => $_SERVER['LDAP_SN'],
+//     'email' => strtolower($_SERVER['LDAP_MAIL'])
+//   );
+//   return $user_info;
+// }
