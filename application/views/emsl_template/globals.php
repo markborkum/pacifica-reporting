367a71cc518af8265ba881c16977db9078309ceb
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-04-04 16:19:32 -0700

    Fixed load order conflict between BootStrap and JQueryUI. Bootstrap needs to load after plain old JQuery, but before JQueryUI to prevent weird styling craziness.

--- application/views/emsl_template/globals.php
+++ application/views/emsl_template/globals.php
@@ -5,6 +5,8 @@
 
     <!-- Base jquery -->
     <script src="/resources/scripts/jquery/jquery-1.11.2.js" type="text/javascript"></script>
+    <script src="/resources/scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
     <script src="/resources/scripts/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="/resources/scripts/jquery-ui/jquery-ui.css" />
 
