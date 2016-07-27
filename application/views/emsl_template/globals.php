8b793d82d380f8eee4842a824b2aa77d4562d932
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-07-27 11:08:49 -0700

    Adding placeholder favicon files

--- application/views/emsl_template/globals.php
+++ application/views/emsl_template/globals.php
@@ -1,7 +1,12 @@
 
     <!-- start global inserts -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/vnd.microsoft.icon" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- Base jquery -->
     <script src="/resources/scripts/jquery/jquery-1.11.2.js" type="text/javascript"></script>
