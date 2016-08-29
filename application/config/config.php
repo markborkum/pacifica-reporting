f398d602eb726ceff52e9a05ef2888409d694ae4
Ken Auberry <kauberry@mac.com>
2016-08-29 16:23:01 -0700

    Fix Issue #3 Merge configs across dev/test/prod (#4)
    Changes to config.php files to consolidate rooted/non-rooted changes across site configs

--- application/config/config.php
+++ application/config/config.php
@@ -26,10 +26,11 @@ date_default_timezone_set('America/Los_Angeles');
 |
 */
 $protocol = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ? "https" : "http";
-$basedir = '/myemsl/reporting';
+$rooted = isset($_SERVER["CI_ROOTED"]) && $_SERVER["CI_ROOTED"] == true ? true : false;
+$basedir = $rooted ? '' : '/myemsl/reporting';
 $config['base_url']	= "{$protocol}://".$_SERVER["SERVER_NAME"].$basedir;
 $config['base_dir'] = $basedir;
-
+$config['local_resources_folder'] = "application/resources";
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -40,7 +41,7 @@ $config['base_dir'] = $basedir;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = $rooted ? '' : 'index.php';
 
 /*
 |--------------------------------------------------------------------------
