55b27be3099ec08feba8a311e1f02a04d9784334
Ken Auberry <kauberry@mac.com>
2018-08-16 14:22:58 -0700

    More branch cleanup (#125)
    * Updating config to better deal with local setup
    
    * Removing debug code
    
    * Updating config to better deal with local setup
    
    * Removing debug code
    
    * Removing debug code (+3 squashed commits)
    Squashed commits:
    [d498924] Updating config to better deal with local setup
    [81c6c5b] Removing debug code
    [561f4f1] Updating config to better deal with local setup
    
    Removing debug code
    
    Updating config to better deal with local setup
    
    Removing debug code
    
    * Somehow wiped out a batch of changes
    
    * Bunch of cleanup to work better with Docker
    
    Updating config to better deal with local setup
    
    * Style fixes

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -10,7 +10,7 @@
  * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @link     http://github.com/EMSL-MSC/pacifica-reporting
  */
- defined('BASEPATH') OR exit('No direct script access allowed');
+ defined('BASEPATH') or exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------
 | AUTO-LOADER
@@ -61,12 +61,12 @@ $autoload['packages'] = array();
 |
 | Prototype:
 |
-|	$autoload['libraries'] = array('database', 'email', 'session');
+|   $autoload['libraries'] = array('database', 'email', 'session');
 |
 | You can also supply an alternative library name to be assigned
 | in the controller:
 |
-|	$autoload['libraries'] = array('user_agent' => 'ua');
+|   $autoload['libraries'] = array('user_agent' => 'ua');
 */
 $autoload['libraries'] = array();
 
@@ -81,7 +81,7 @@ $autoload['libraries'] = array();
 |
 | Prototype:
 |
-|	$autoload['drivers'] = array('cache');
+|   $autoload['drivers'] = array('cache');
 */
 $autoload['drivers'] = array();
 
@@ -91,7 +91,7 @@ $autoload['drivers'] = array();
 | -------------------------------------------------------------------
 | Prototype:
 |
-|	$autoload['helper'] = array('url', 'file');
+|   $autoload['helper'] = array('url', 'file');
 */
 $autoload['helper'] = array();
 
@@ -101,7 +101,7 @@ $autoload['helper'] = array();
 | -------------------------------------------------------------------
 | Prototype:
 |
-|	$autoload['config'] = array('config1', 'config2');
+|   $autoload['config'] = array('config1', 'config2');
 |
 | NOTE: This item is intended for use ONLY if you have created custom
 | config files.  Otherwise, leave it blank.
@@ -115,7 +115,7 @@ $autoload['config'] = array('pacifica');
 | -------------------------------------------------------------------
 | Prototype:
 |
-|	$autoload['language'] = array('lang1', 'lang2');
+|   $autoload['language'] = array('lang1', 'lang2');
 |
 | NOTE: Do not include the "_lang" part of your file.  For example
 | "codeigniter_lang.php" would be referenced as array('codeigniter');
@@ -129,11 +129,11 @@ $autoload['language'] = array();
 | -------------------------------------------------------------------
 | Prototype:
 |
-|	$autoload['model'] = array('first_model', 'second_model');
+|   $autoload['model'] = array('first_model', 'second_model');
 |
 | You can also supply an alternative model name to be assigned
 | in the controller:
 |
-|	$autoload['model'] = array('first_model' => 'first');
+|   $autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array();
